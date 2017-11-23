33ea71bf757309bb668578830753e1563b4eaed3
beqa calani <beqacalani@gmail.com>
2017-11-24 00:14:31 +0400

    commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+ 
+  $("#mainslider").owlCarousel({
+ 
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+
+  // rooms inner slider
+   $("#roomslider").owlCarousel({
+ 
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+
+   $( ".date" ).datepicker();
+ 
+});
